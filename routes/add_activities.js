e1263ff058acdbc0970a544779863e7917fa6574
Melissa Boyer <204939@supinfo.com>
2017-08-23 21:44:57 +0200

    #7 Add 1st activity, new activity with parent, delete one activity, delete the project
    BUG : row duplication when refresh

--- routes/add_activities.js
+++ routes/add_activities.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 
-neo4j  = require('neo4j-driver').v1;
-driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '16038943Brookes'));
+neo4j  = require('neo4j-driver').v1,
+driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '16038943Brookes')),
 sess   = driver.session();
 
 var activityArray = [];
